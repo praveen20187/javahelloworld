b032b12fb98d06a2b49c37289ce82acdb844ec31
praveen20187 <praveen20187@gmail.com>
2015-11-29 00:49:12 -0800

    initial commit

---
+++ JavaHelloWorld.java
@@ -0,0 +1,9 @@
+pulic class JavaHelloWorld
+{
+	public static void main (String [] args)
+	{
+		System.out.println("Java Hello World from my ubuntu terminal ");
+		System.out.println(" Hello again ..........");
+	}
+}
+
